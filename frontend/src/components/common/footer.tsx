70fbabca80113dd8adf8354096da0c7cd8f8df56
Nguyen Tuan Huy <githubhuy29082004@gmail.com>
2026-04-10 18:13:35 +0700

    11111

--- frontend/src/components/common/footer.tsx
+++ frontend/src/components/common/footer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { GradientText } from "../ui/gradient-text";
 export default function Footer() {
   return (
     <div className="w-full h-80 bg-gray-800 text-white flex items-center justify-center">
-      © 2023 Bé Gấu. All rights reserved.
+      © 2023 Bé Gấu. All rights reserved...
+      <GradientText className="text-[12px] f"></GradientText>
     </div>
   );
 }
