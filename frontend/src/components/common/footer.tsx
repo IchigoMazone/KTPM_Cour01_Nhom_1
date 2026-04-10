209a0c5151a18d1d5d5350245b609575d100509a
Nguyen Tuan Huy <githubhuy29082004@gmail.com>
2026-04-10 20:10:14 +0700

    1111111111111

--- frontend/src/components/common/footer.tsx
+++ frontend/src/components/common/footer.tsx
@@ -1,10 +1,32 @@
+"use client"
 import React from "react";
 import { GradientText } from "../ui/gradient-text";
+
 export default function Footer() {
   return (
-    <div className="w-full h-80 bg-gray-800 text-white flex items-center justify-center">
-      © 2023 Bé Gấu. All rights reserved...
-      <GradientText className="text-[12px] f"></GradientText>
+    // <div className="w-full h-80 bg-gray-800 text-white flex items-center justify-center">
+    //   © 2023 Bé Gấu. All rights reserved...
+    // </div>
+    <div className="grid grid-rows-[1fr_5fr_1fr] w-full h-80 bg-gray-100 text-black p-[20px]">
+      <div className="flex ">
+        <span>Liên hệ với chúng tôi qua mạng xã hội</span>
+        <div className="w-[20px] h-[20px]">
+          <img src="/logo-facebook.svg" alt="" />
+        </div>
+        <div>
+        
+        </div>
+      </div>
+      <div className="flex p-[20px] ">
+        <div className="flex-1">
+          <div>
+            
+          </div>
+        </div>
+        <div className="flex-1"></div>
+        <div className="flex-1"></div>
+      </div>
+      <div></div>
     </div>
   );
 }
