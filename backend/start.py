572eff3bab0ebf7d4d03f3a2fb993eec91b83fa1
Nguyen Tuan Huy <githubhuy29082004@gmail.com>
2026-05-24 10:18:50 +0700

    uuu

--- backend/start.py
+++ backend/start.py
@@ -2,4 +2,9 @@ import uvicorn
 
 if __name__ == "__main__":
     print("Khởi động FastAPI Backend Server...")
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
